aabf0a64c8ff331557be7c7e7cee7775199cf2b5
Ben Sweeney <bends11@vt.edu>
2023-04-29 10:16:07 -0400

    Update state to use a Map of Items

--- src/app/state/items/items.actions.ts
+++ src/app/state/items/items.actions.ts
@@ -3,8 +3,8 @@ import { Item } from "../models/item";
 
 export const init = createAction('[Items] Initialize Items');
 
-export const getItemsSuccess = createAction('[Items Effects] Get Items Success', props<{ items: Item[]}>());
+export const getItemsSuccess = createAction('[Items Effects] Get Items Success', props<{ items: Map<string, Item>}>());
 
 export const getItemsError = createAction('[Items Effects] Get Items Error');
 
-export const getItemsDetailsSuccess = createAction('[Items Effects] Get Item Details Success', props<{ items: Item[] }>());
+export const getItemsDetailsSuccess = createAction('[Items Effects] Get Item Details Success', props<{ items: Map<string, Item> }>());
